e218829c9766b9b2a6e2b4e2b119c29b8becb49b
Andrija Arsenic <43091463+ArsaBB@users.noreply.github.com>
2019-01-23 13:18:38 +0100

    Add files via upload

---
+++ moneyTracker2.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM moneyshema.tabela
+LIMIT 0, 1000
+
+-- Date: 2019-01-23 13:18
+*/
+INSERT INTO `tabela` (`tabela_id`,`datum`,`kategorija`,`komentar`,`suma`,`valuta`) VALUES (1,'2025-01-20 18:00:00','prevoz','voznja od Usca do Delte','1000','rsd');
+INSERT INTO `tabela` (`tabela_id`,`datum`,`kategorija`,`komentar`,`suma`,`valuta`) VALUES (2,'2023-04-20 19:00:00','shopping','kupovina u TC Stadion','130','eur');
